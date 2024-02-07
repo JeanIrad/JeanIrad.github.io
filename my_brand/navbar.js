32c19965c97aaeba05c25310c405fcc582d771c5
JeanIrad <jado.milton@gmail.com>
2024-02-07 23:02:29 +0200

    commiting about page

--- my_brand/navbar.js
+++ my_brand/navbar.js
@@ -1,17 +1,6 @@
 const bars = document.querySelector(".fa-bars");
 const navList = document.querySelector("nav ul");
 bars.onclick = () => {
-  //   navList.style.top = 0;
   navList.classList.toggle("close");
-};
-
-const moon = document.querySelector(".fa-moon");
-
-moon.onclick = function () {
-  navList.style.color = "#151733";
-  this.classList.remove("fa-moon");
-  bars.style.color = "#151733";
-  this.style.color = "#151733";
-  this.classList.add("fa-sun");
-  document.body.style.background = "#eee";
+  alert("hello");
 };
