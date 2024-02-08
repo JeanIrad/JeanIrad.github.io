d4a44351da7134837b96e19513d8dcf283876a31
JeanIrad <jado.milton@gmail.com>
2024-02-08 08:37:58 +0200

    commiting portfolio

--- my_brand/navbar.js
+++ my_brand/navbar.js
@@ -2,5 +2,5 @@ const bars = document.querySelector(".fa-bars");
 const navList = document.querySelector("nav ul");
 bars.onclick = () => {
   navList.classList.toggle("close");
-  alert("hello");
+  // alert("hello");
 };
