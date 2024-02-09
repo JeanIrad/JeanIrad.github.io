2bb2951fd3ad9f3b1805c70feb2b4f6961588f83
JeanIrad <jado.milton@gmail.com>
2024-02-09 18:00:33 +0200

    a unislider

--- my_brand/js/slider.js
+++ my_brand/js/slider.js
@@ -1,1 +1,23 @@
 const portfolioImage = document.querySelector("#portfolio__image");
+const portfolioTitle = document.querySelector("#portfolio__title");
+const sliderDivs = document.querySelectorAll(".slider div");
+let index = 0;
+const nextVal = [
+  {
+    title: "Game development",
+    image: "./images/mine/portfolio/image2.png",
+  },
+  {
+    title: "Data Analytics",
+    image: "./images/mine/portfolio/image3.png",
+  },
+];
+setInterval(() => {
+  index++;
+  portfolioImage.src = nextVal[index].image;
+  portfolioTitle.textContent = nextVal[index].title;
+  sliderDivs[index].style.background = "#cfa511";
+  if (index > 2) {
+    index = 0;
+  }
+}, 3000);
