0f5d6d0fd369e33378a0b2119a123807335ca57c
JeanIrad <jado.milton@gmail.com>
2024-03-25 11:20:32 +0200

    retouching on state implementation

--- my_brand/scripts/handleLikes.js
+++ my_brand/scripts/handleLikes.js
@@ -1,6 +1,7 @@
 const App = () => {
+  const blogId = new URLSearchParams(window.location.search).get("id");
   const [likes, setLikes] = React.useState(() => {
-    const storedVal = JSON.parse(localStorage.getItem("likes")) ?? 0;
+    const storedVal = JSON.parse(localStorage.getItem(`likes_${blogId}`)) ?? 0;
     return storedVal;
   });
   //   if (!localStorage.getItem("likes")) {
@@ -8,19 +9,16 @@ const App = () => {
   //   }
   const handleClicks = () => {
     setLikes(likes + 1);
-    localStorage.setItem("likes", JSON.stringify(likes + 1));
+    localStorage.setItem(`likes_${blogId}`, JSON.stringify(likes + 1));
   };
   return (
     <div className="likes_container">
-      <button
-        id="likeBtn"
-        onClick={() =>
-          handleClicks(JSON.parse(localStorage.getItem("likes")) + 1)
-        }
-      >
+      <button id="likeBtn" onClick={() => handleClicks()}>
         Like👍
       </button>
-      <span id="numLikes">{JSON.parse(localStorage.getItem("likes"))}</span>
+      <span id="numLikes">
+        {JSON.parse(localStorage.getItem(`likes_${blogId}`))}
+      </span>
     </div>
   );
 };
